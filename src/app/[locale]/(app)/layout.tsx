5eaaea4ff664a2aadf493d586561b1fb26be63d4
wheeliechamp <lets.spunky@gmail.com>
2025-07-05 04:32:55 +0000

    AppLayout.qualifictaions が日本語になっていません

--- src/app/[locale]/(app)/layout.tsx
+++ src/app/[locale]/(app)/layout.tsx
@@ -30,7 +30,7 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
     },
     {
       href: '/projects',
-      label: t('qualifications'),
+      label: t('projects'),
       icon: BookMarked,
     },
   ];
